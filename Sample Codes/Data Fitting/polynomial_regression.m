f7d913ffd3251cff83bb39e4e6ade71c900e6312
efeerenc <efeeren.c@gmail.com>
2022-11-21 15:14:58 +0300

    update repo (hws etc.)

--- Sample Codes/Data Fitting/polynomial_regression.m
+++ Sample Codes/Data Fitting/polynomial_regression.m
@@ -12,7 +12,7 @@ set(groot, 'defaultLegendInterpreter','latex');
 
 %% Parameters
 order = 5;
-fitting_order = 2;
+fitting_order = 5;
 n = 500;
 x_min = 0;
 x_max = 10; 
@@ -24,9 +24,11 @@ x_axis = linspace(x_min, x_max, n);
 coeff = randn(1, order+1);
 
 dpts = polyval(coeff, x_axis)/100 + noise_weight*randn(1, n);
+data = [2, 3; 4.5, 7; 8, 1];
 
 figure;
-plot(x_axis, dpts, "*");
+%plot(x_axis, dpts, "*");
+plot(data(:, 1), data(:, 2), "*")
 title("Polynomial regression to a given data");
 xlabel("$x$");
 ylabel("$y$");
@@ -34,13 +36,15 @@ hold on;
 
 %% Fit to a line
 
-X = zeros(n, fitting_order+1);
+%X = zeros(n, fitting_order+1);
 
 for i = 1:(fitting_order+1)
-    X(:, i) = transpose(x_axis).^(fitting_order+1-i);
+    %X(:, i) = transpose(x_axis).^(fitting_order+1-i);
+    X(:, i) = transpose(data(:, 1)).^(fitting_order+1-i);
 end
 
-y = transpose(dpts);
+%y = transpose(dpts);
+y = data(:, 2);
 
 p = (transpose(X)*X)\transpose(X)*y; % first term is the pseudoinverse
 
